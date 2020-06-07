20dbebee83882a6ae98f728729418d29c48bd520
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-06-07 22:34:49 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
+def roll_call_dwarves(names)
+  counter = 0
+  while counter < names.length
+    names.each do |name| puts "#{counter+1}. #{name}" end
+      counter +=1
+    end
+  end
+  ######################################################################################################################
+  def summon_captain_planet(planeteer)
+    counter = 0
+    cap = []
+    while counter < planeteer.length
+      planeteer.each do |name|
+        cap << "#{planeteer[counter].capitalize}!"
+        counter += 1
+      end
+    end
+    cap
+  end
+  ######################################################################################################################
+  def long_planeteer_calls(array)
+    array.any? do |call|
+      call.length > 4
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+    end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  end
+  ######################################################################################################################
+  def find_the_cheese(snacks)
+    # the array below is here to help	  # the array below is here to help
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    snacks.find do |item|
+      cheese_types.include?(item)
+    end
+  end
