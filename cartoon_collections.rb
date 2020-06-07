439b5b85c1fbc5459515e4f5473d1360b5129ee9
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-07 15:55:35 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,44 +1,40 @@
-dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
-#planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-
-#cheese_types = "cheddar"`, `"gouda"`, and
-#`"camembert"
-
 def roll_call_dwarves(array)# code an argument here
   i = 0
   while i < array.length
-    puts "#{i = 1}. #{array[i]}"
+    puts "#{i + 1}. #{array[i]}"
     i = i += 1
     end
 end
-roll_call_dwarves(dwarves)
 
-planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
 def summon_captain_planet(array)
   i = 0
+  new_array = []
   while i < array.length
-    new_array << array[i].upcase + "!"
+    new_array << array[i].capitalize + "!"
     i += 1
   end
   new_array
 end
-summon_captain_planet(planeteer_calls)
+
 
 def long_planeteer_calls(calls)
-  i = 0
-  if array.any? {|i|} i.length > 4
-    return true
-  else
-    return false
-    i = i + 1
-  end
+  calls.any? {|call| call.length > 4}#planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 end
-long_planeteer_calls(planeteer_calls)
+
 
 #cheese_types = "cheddar", "gouda"`, and
 #"camembert"
 #cheese_types = "cheddar", "gouda"`, and
 #"camembert"
 #ingredients = ["garlic", "rosemary", "bread"]
-def find_the_cheese(array)
+def find_the_cheese(no_cheese)
+  cheese_types = ["cheddar", "gouda",
+  "camembert"]
+  no_cheese.find {|cheese| cheese_types.include?(cheese)}
+end
+
+  #array.each do |element|
+#  if cheese_types.include?(element) == "cheddar"
+    #then return "cheddar"
+  #else return nil
